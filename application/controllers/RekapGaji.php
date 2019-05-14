610b8c9eb9e046ec0a64eae80809f54b16ae9852
Demaspira Aulia <demaspiraa@gmail.com>
2019-05-14 12:16:21 +0700

    fix on rekapgaji on macos

--- application/controllers/RekapGaji.php
+++ application/controllers/RekapGaji.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class RekapGaji extends CI_Controller {
+class Rekapgaji extends CI_Controller {
 
   public function __construct()
   {
